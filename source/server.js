242d32ec7bc4779af6d8bfd0b3dd874a6a40b076
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2022-02-01 20:52:15 +0530

    Dynamic content rendering

--- source/server.js
+++ source/server.js
@@ -33,20 +33,24 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+//This function delivers the home page.
 app.get('/', function (req, res){
-	if(req.session.loggedIn== 'true'){
+	res.sendFile(htmlPath + "index.html");
+	//Below code is as a reference for session checking.
+	/*if(req.session.loggedIn== 'true'){
 		//Send session data to Ranga's front end using REST (JSON).
 		res.send("Welcome " + req.session.firstName + " " + req.session.email);
 	}else{
 		res.sendFile(htmlPath + "index.html");
-	}
+	}*/
 });
 
+//This function delivers the sign in form.
 app.get('/sign-in', function (req, res){
 	res.sendFile(htmlPath + "sign-in.html");
 });
 
+//This function delivers the sign up form.
 app.get('/sign-up', function (req, res){
 	res.sendFile(htmlPath + "sign-up.html");
 });
@@ -95,11 +99,12 @@ app.post("/sign-up-process", function(req, res){
 	});
 });
 
+//This function delivers a sample data presentation page.
 app.get('/view-users', function(req, res) {
 	res.sendFile(htmlPath + "view-users.html");
 })
 
-//An API which sends data to the caller.
+//An API which sends user data to the caller.
 app.get('/view-users-process', function (req, res){
 	//Below is a callback function since node.js doesn't support returning values from functions.
 	//Add session authentication as well.
@@ -108,6 +113,14 @@ app.get('/view-users-process', function (req, res){
 		res.json(result);	
 	});
 });
+
+//An API which sends session data to the caller.
+app.get('/get-session', function(req, res) {
+	var object= {loggedIn: req.session.loggedIn, userId: req.session.userId, email: req.session.email, 
+		firstName: req.session.firstName}
+	console.log(object);
+	res.json(object);
+})
 
 //Handle invalid URLs.
 app.all('*', function(req, res) {
