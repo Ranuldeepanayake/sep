96dcc728aa697a8ea6b7541c7ba1d1d0d7991e68
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2022-04-02 13:18:09 +0530

    Unit testing

--- source/test/backend-supplier.js
+++ source/test/backend-supplier.js
@@ -1,24 +1,29 @@
 //const mocha= require('mocha');
 const chai= require('chai');
 //chai.config.showDiff = true;
-const supplierCode = require('../js/supplier.js');
+const supplier = require('../js/supplier.js');
 
+//Unit test to check if the supplier.showVisitor() dataset is defined. It may be undefined
+//if there are database connectivity issues etc.
 describe('Testing supplier.showVisitor()', function(){
-   
-    let supplier = supplierCode;
     
-    it('Check if supplier data can be viewed by a visitor>.', function(){
+    it('Test if the list of suppliers can be queried from the backend to be shown to a visitor.', function(){
         supplier.showSuppliersVisitor(function (result){
             //chai.assert.isString(result, "These are strings");
             //chai.assert.isNotArray(result, "This is an array.")
             chai.assert.isDefined(result, "Data set is empty!")
         });
     })
-    
-   /* it('check for a dish in menu.', function (){
-        let dish= chef.checkMenu()
-        assert.oneOf(dish, chef.dishes)
+});
 
-    });*/
+describe('Testing supplier.getItemsList()', function(){
     
+    it('Test if the list of items for a particular supplier can be queried from the backend to be shown to a visitor/customer.', 
+    function(){
+        supplier.getItemsList('1', function (result){
+            //chai.assert.isString(result, "These are strings");
+            //chai.assert.isNotArray(result, "This is an array.")
+            chai.assert.isDefined(result, "Data set is empty!")
+        });
+    })
 });
